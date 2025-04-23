9e183e67632eefc04d218e7aa53e745e01a2bcdc
Rishita Meharishi <rmeharishi@mail.sfsu.edu>
2025-04-22 20:28:16 -0700

    code for 2 cp#3 files

--- dgemv-basic.cpp
+++ dgemv-basic.cpp
@@ -1,11 +1,26 @@
-const char* dgemv_desc = "Basic implementation of matrix-vector multiply.";
 
+const char* dgemv_desc = "Basic implementation of matrix-vector multiply.";
 /*
  * This routine performs a dgemv operation
- * Y :=  A * X + Y
+ * Y := A * X + Y
  * where A is n-by-n matrix stored in row-major format, and X and Y are n by 1 vectors.
  * On exit, A and X maintain their input values.
  */
 void my_dgemv(int n, double* A, double* x, double* y) {
-   // insert your code here: implementation of basic matrix multiply
+
+    // go each row of the matrix A
+    for (int i = 0; i < n; i++) {
+        double temp = 0.0; 
+
+        // Temporary variable to store the dot product
+        // Compute the dot product of the i-th row of A with vector x
+        for (int j = 0; j < n; j++) {
+            temp += A[i * n + j] * x[j];
+        }
+        // Add the result to the corresponding element of vector y
+        // same logic code could be a line shorter by using the following line?
+        //y[i]+=A[i*n+j]*x[j];
+        y[i] += temp;
+    }
 }
+
--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -6,7 +6,23 @@ const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
  * where A is n-by-n matrix stored in row-major format, and X and Y are n by 1 vectors.
  * On exit, A and X maintain their input values.
  */
+
+// TODO: You may need to tweak your basic VMM code so that it actually is vectorizable by the compiler. 
+//Check the compiler vectorization report (report.txt) to look for evidence that the code vectorized or not. 
+
 void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your code here: implementation of vectorized vector-matrix multiply
+    // go each row of the matrix A
+    for (int i = 0; i < n; i++) {
+        double temp = y[i]; 
 
+        // Temporary variable to store the dot product
+        // Compute the dot product of the i-th row of A with vector x
+        
+        for (int j = 0; j < n; j++) {
+            temp += A[i * n + j] * x[j];
+        }
+        y[i] += temp;
+    }
 }
+
